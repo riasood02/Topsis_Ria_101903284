613f98ce1b5219ea5bf0b8b60baafe60110dc177
riasood02 <67365618+riasood02@users.noreply.github.com>
2022-02-27 22:42:42 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Ria_101903284',        
+  packages = ['Topsis_Ria_101903284'],  
+  version = 'v1.2',      
+  license='MIT',       
+  description = 'Topsis Score and Rank generator',
+  author = 'Ria Sood',                
+  author_email = 'rsood1_be19@thapar.edu',      
+  url = 'https://github.com/riasood02/Topsis_Ria_101903284',   
+  download_url = '',    
+  keywords = ['topsis','topsis score','rank','Thapar'],
+  install_requires=['numpy','pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
